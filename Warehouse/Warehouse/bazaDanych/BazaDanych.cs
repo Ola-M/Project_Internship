59cfda81f9d17fbb104d5696fb8f2372d7c44d64
Bartosz Skrabka <bartosz.skrabka@grandhomegroup.com>
2020-07-17 12:46:31 +0200

    xx

---
+++ Warehouse/Warehouse/bazaDanych/BazaDanych.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Warehouse
+{
+    class BazaDanych
+    {
+        private SqlConnection connection = new SqlConnection("Data Source = MSSQLLocalDB; Initial Catalog = warehouseDatabase; Integrated Security = True; MultipleActiveResultSets=True;Application Name = EntityFramework");
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+        public SqlConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
